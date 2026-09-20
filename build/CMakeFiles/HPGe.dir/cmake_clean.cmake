95881e9b6a4d30ae10767fc947bd7d511304d344
Giacomo Cervi <giacomo.cervi.2@studenti.unipd.it>
2026-09-20 21:49:50 +0200

    Version 3

---
+++ build/CMakeFiles/HPGe.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HPGe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/HPGe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/sim.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sim.dir/sim.cc.o"
+  "CMakeFiles/sim.dir/src/HPGeActionInitialization.cc.o"
+  "CMakeFiles/sim.dir/src/HPGeDetectorConstruction.cc.o"
+  "CMakeFiles/sim.dir/src/HPGeMessenger.cc.o"
+  "CMakeFiles/sim.dir/src/HPGePhysicsList.cc.o"
+  "CMakeFiles/sim.dir/src/HPGePrimaryGenerator.cc.o"
+  "CMakeFiles/sim.dir/src/HPGeRunAction.cc.o"
+  "CMakeFiles/sim.dir/src/HPGeSensitiveDetector.cc.o"
+  "sim"
+  "sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
