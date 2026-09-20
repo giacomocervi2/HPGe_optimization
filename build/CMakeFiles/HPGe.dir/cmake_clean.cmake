fc2e0ea4ccf0ec43011760d2c28daedb90199698
giacomocervi2 <giacomo.cervi.2@studenti.unipd.it>
2026-09-20 21:50:29 +0200

    Delete build directory

--- build/CMakeFiles/HPGe.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/HPGe"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/HPGe.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
